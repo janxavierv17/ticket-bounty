de2df9265c2ffe0e343f22c52cafd8373e44e790
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2025-01-16 00:06:07 +1100

    repo configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,20 +7,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-	{
-		plugins: {
-			"simple-import-sort": simpleImportSort,
-		},
-		rules: {
-			"simple-import-sort/imports": ["error", { groups: [["^\\u0000", "^node:", "^@?\\w", "^", "^\\."]] }],
-			"simple-import-sort/exports": "error",
-		},
-	},
+  {
+    // Ignore the `.next` folder
+    ignores: [".next"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        { groups: [["^\\u0000", "^node:", "^@?\\w", "^", "^\\."]] },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+    ignores: [".next"],
+  },
 ];
 
 export default eslintConfig;
